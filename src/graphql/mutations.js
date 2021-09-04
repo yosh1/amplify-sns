a45a1fec3fe0429eac167d3a84f5ec8ebb57e2e1
yosh1 <yoppy1125yoppy@gmail.com>
2021-09-04 17:00:43 +0900

    Add functions

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,12 +1,9 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createPost = /* GraphQL */ `
-  mutation CreatePost(
-    $input: CreatePostInput!
-    $condition: ModelPostConditionInput
-  ) {
-    createPost(input: $input, condition: $condition) {
+export const createPostAndTimeline = /* GraphQL */ `
+  mutation CreatePostAndTimeline($content: String!) {
+    createPostAndTimeline(content: $content) {
       type
       id
       content
@@ -29,24 +26,38 @@ export const deletePost = /* GraphQL */ `
     }
   }
 `;
-export const createFollowRelationship = /* GraphQL */ `
-  mutation CreateFollowRelationship(
-    $input: CreateFollowRelationshipInput!
-    $condition: ModelFollowRelationshipConditionInput
-  ) {
-    createFollowRelationship(input: $input, condition: $condition) {
-      followeeId
-      followerId
-      timestamp
-    }
-  }
-`;
 export const deleteFollowRelationship = /* GraphQL */ `
   mutation DeleteFollowRelationship(
     $input: DeleteFollowRelationshipInput!
     $condition: ModelFollowRelationshipConditionInput
   ) {
     deleteFollowRelationship(input: $input, condition: $condition) {
+      followeeId
+      followerId
+      timestamp
+    }
+  }
+`;
+export const createPost = /* GraphQL */ `
+  mutation CreatePost(
+    $input: CreatePostInput!
+    $condition: ModelPostConditionInput
+  ) {
+    createPost(input: $input, condition: $condition) {
+      type
+      id
+      content
+      owner
+      timestamp
+    }
+  }
+`;
+export const createFollowRelationship = /* GraphQL */ `
+  mutation CreateFollowRelationship(
+    $input: CreateFollowRelationshipInput!
+    $condition: ModelFollowRelationshipConditionInput
+  ) {
+    createFollowRelationship(input: $input, condition: $condition) {
       followeeId
       followerId
       timestamp
